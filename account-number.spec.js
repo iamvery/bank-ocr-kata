589aaa5f72544df4ffa7c869b3fda1e5669398ab
Jay Hayes <ur@iamvery.com>
2017-06-29 16:40:27 -0500

    Fix variable initializations in test
    Without explicit var, let, or const these values become GLOBAL! BAD NEWS

--- account-number.spec.js
+++ account-number.spec.js
@@ -1,8 +1,8 @@
 const { numbers, convertToValue } = require('./account-number')
 
 it('converts string numeric representation to numeric', () => {
-  value_one = convertToValue(numbers.one)
-  value_two = convertToValue(numbers.two)
+  let value_one = convertToValue(numbers.one)
+  let value_two = convertToValue(numbers.two)
 
   expect(value_one).toEqual(1)
   expect(value_two).toEqual(2)
